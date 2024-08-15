25860ce7e5d7e7d2ee465232c7bfb9f106391335
tzfun <beifengtz@qq.com>
2024-08-15 15:27:27 +0800

    build 打包体积优化 #26

---
+++ app/package.js
@@ -0,0 +1,44 @@
+// mdi字体体积优化：https://www.shedloadofcode.com/blog/reduce-material-design-icons-font-to-7kb-and-automate-with-pyautogui
+import path from 'path'
+import fs from 'fs'
+
+function deleteFile(fileOrDirName) {
+    let stat = fs.statSync(fileOrDirName)
+    if (stat.isDirectory()) {
+        for (let f of fs.readdirSync(fileOrDirName)) {
+            let filePath = path.join(fileOrDirName, f)
+            deleteFile(filePath)
+        }
+        fs.rmdirSync(fileOrDirName)
+    } else {
+        fs.unlinkSync(fileOrDirName)
+        console.log(`deleted ==> ${fileOrDirName}`)
+    }
+}
+
+function deleteFileWithPattern(dir, regexp) {
+    let replaceMap = {}
+    for (let f of fs.readdirSync(dir)) {
+        if (f.match(regexp)) {
+            fs.unlinkSync(path.join(dir, f))
+            console.log(`deleted ==> ${dir}/${f}`)
+            if (f.endsWith("css") || f.endsWith("js")) {
+                replaceMap[f] = f + ".gz"
+            }
+        }
+    }
+    return replaceMap
+}
+
+function replaceFile(file, replaceMap) {
+    let data = fs.readFileSync(file, {encoding: 'utf-8'})
+    for (let key in replaceMap) {
+        data = data.replaceAll(key, replaceMap[key])
+        console.log(`replaced ${key} to ${replaceMap[key]}`)
+    }
+    fs.writeFileSync(file, data, {encoding: 'utf-8'})
+}
+
+
+const replaceMap = deleteFileWithPattern('./dist/assets', /^.*\.(ttf|eot|woff)$/)
+// replaceFile('./dist/index.html', replaceMap)
--- app/src-tauri/src/api/role.rs
+++ app/src-tauri/src/api/role.rs
@@ -1,9 +1,16 @@
 use crate::error::LogicError;
 use crate::etcd;
+use crate::transport::user::SerializablePermission;
 
 #[tauri::command]
 pub async fn role_list(session: i32) -> Result<Vec<String>, LogicError> {
     let connector = etcd::get_connector(&session)?;
     let roles = connector.role_list().await?;
     Ok(roles)
+}
+#[tauri::command]
+pub async fn role_get_permissions(session: i32, role: String) -> Result<Vec<SerializablePermission>, LogicError> {
+    let connector = etcd::get_connector(&session)?;
+    let permissions = connector.role_get_permissions(role).await?;
+    Ok(permissions)
 }
