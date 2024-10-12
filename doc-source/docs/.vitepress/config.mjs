c03e718bca7f4514217bb2484e6ff94a1a8c75f0
beifengtz <beifengtz@qq.com>
2024-10-12 15:50:28 +0800

    【doc】update docs head

--- doc-source/docs/.vitepress/config.mjs
+++ doc-source/docs/.vitepress/config.mjs
@@ -1,36 +1,39 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress'
 import {enConfig} from "./config/en.js";
 import {zhConfig} from "./config/zh.js";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Etcd Workbench",
-  description: "A beautiful and lightweight ETCD client",
-  base:"/etcd-workbench/",
-  themeConfig: {
-    logo: '/images/logo.png',
-    siteTitle: 'Etcd Workbench',
-
-    sidebar: [],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/tzfun/etcd-workbench' },
+    title: "Etcd Workbench",
+    description: "A beautiful and lightweight ETCD client",
+    base: "/etcd-workbench/",
+    head: [
+        ['link', {rel: 'icon', type: 'image/png', href: '/etcd-workbench/images/ico.png'}],
     ],
+    themeConfig: {
+        logo: '/images/logo.png',
+        siteTitle: 'Etcd Workbench',
 
-    // search: {
-    //   provider: 'local'
-    // }
-  },
-  locales: {
-    root: {
-      label: 'English',
-      lang: 'en',
-      ...enConfig
+        sidebar: [],
+
+        socialLinks: [
+            {icon: 'github', link: 'https://github.com/tzfun/etcd-workbench'},
+        ],
+
+        // search: {
+        //   provider: 'local'
+        // }
     },
-    zh: {
-      label: '中文',
-      lang: 'zh',
-      ...zhConfig
+    locales: {
+        root: {
+            label: 'English',
+            lang: 'en',
+            ...enConfig
+        },
+        zh: {
+            label: '中文',
+            lang: 'zh',
+            ...zhConfig
+        },
     },
-  },
 })
