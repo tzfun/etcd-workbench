d08c1729d953628b61447536731c50167a3914b2
beifengtz <beifengtz@qq.com>
2024-10-12 15:29:59 +0800

    【doc】update docs

--- docs/assets/chunks/@localSearchIndexzh.cVud1dAQ.js
+++
@@ -1,1 +0,0 @@
-const e='{"documentCount":0,"nextId":0,"documentIds":{},"fieldIds":{"title":0,"titles":1,"text":2},"fieldLength":{},"averageFieldLength":[],"storedFields":{},"dirtCount":0,"index":[],"serializationVersion":2}';export{e as default};
